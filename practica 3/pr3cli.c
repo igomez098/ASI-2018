60b5caa446fba9dbdc0bf8c14cc85ea0d0e773c5
Docencia <igomez098@ikasle.ehu.eus>
2018-04-19 09:54:41 +0200

    Avance de la prepractica

--- practica 3/pr3cli.c
+++ practica 3/pr3cli.c
@@ -4,6 +4,8 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/sem.h>
 
 
 #define clave 0x16096153L
@@ -17,11 +19,27 @@ void limpio()
 
 int main(int argc,char*argv[])
 {
-	int id,idsem;
+	int id,idsem,var;
 	char*dir;
+	char prop[]="Hola soy el cliente 1";
+	char msg[255];
+	struct sembuf sum[2]={{0,1,0},{1,1,0}},res[2]={{0,-1,0},{1,-1,0}};
 
 	id=shmget(clave,1024,0666);
 	dir=shmat(id,0,0);
-	idsem=semget(clave,1,0666);
+	idsem=semget(clave,2,0666);
+
+	semop(idsem,&res[0],1);
+	sscanf(dir,"%s",msg);
+	var=*((int*)(dir+sizeof(msg)));
+	printf("El mensaje leido es: %s\n",msg);
+	if(var!=1)
+		var=1;
+	sprintf(dir,"%s",prop);
+	*((int*)(dir+sizeof(prop)))=var;
+	semop(idsem,&sum[0],1);
+	shmdt(dir);
+
+	return 0;
 
 }
--- practica 3/pr3serv.c
+++ practica 3/pr3serv.c
@@ -4,6 +4,8 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/sem.h>
 
 
 #define clave 0x16096153L
@@ -28,23 +30,34 @@ void intro()
 
 int main(int argc,char*argv[])
 {
-	int id,idsem;
+	int id,idsem,numcli=0;
 	char*dir;
-	char msg[]="HOLA";
-	struct sembuf sum={0,1,0},res={0,-1,0};
+	char msg[]="Hola soy el servidor";
+	char nuevo[256];
+	struct sembuf sum[2]={{0,1,0},{1,1,0}},res[2]={{0,-1,0},{1,-1,0}};
 
 
 	id=shmget(clave,1024,IPC_CREAT|0666);
+	idsem=semget(clave,2,IPC_CREAT|0666);
+	semctl(idsem,0,SETVAL,1);
+	semctl(idsem,1,SETVAL,1);
+
+	semop(idsem,&res[0],1);
 	dir=shmat(id,0,0);
 	sprintf(dir,"%s",msg);
-	idsem=semget(clave,1,IPC_CREAT|0666);
-	semctl(idsem,0,SETVAL,1);
-	while(car!='s')
-	{
-		printf("Pulsa s para salir: ");
-		limpio();
-		fgets(car,sizeof(car),stdin);
-	}
+	*((int*)(dir+sizeof(msg)))=numcli;
+	semop(idsem,&sum[0],1);
+
+	sleep(5);
+
+	semop(idsem,&res[0],1);
+	sscanf(dir,"%s",nuevo);
+	numcli=*((int*)(dir+sizeof(nuevo)));
+	semop(idsem,&sum[0],1);
+
+	printf("El mensaje del cliente %d es: %s\n",numcli,nuevo);
+
+	intro();
 	shmdt(dir);
 	shmctl(id,IPC_RMID,NULL);
 
